aebe563cf0cc0ddbddd3b31b8b4cf8f9e790d8cf
tom-finance <51618758+tom-finance@users.noreply.github.com>
2020-07-26 16:43:05 +0200

    added file and updated automation skript

--- R/create_report.R
+++ R/create_report.R
@@ -5,16 +5,16 @@
 ################################################################################
 
 # define filename to be used for output file
-file_name <- paste("Report_Systemindikatoren_", 
-                   format(Sys.Date(), "%d_%m_%Y"), ".html", 
+file_name <- paste("short_seller_analysis_", 
+                   format(Sys.Date(), "%d_%m_%Y"), ".pdf", 
                    sep = "")
 
 # render markdown document and safe to output directory
-rmarkdown::render(input = "G:/Risiko/Anwendungen/Report_Systemindikatoren/report/Markdown_HTML_Indikatoren_ALL.Rmd",
+rmarkdown::render(input = "short_seller_analysis.Rmd",
                   quiet = FALSE, # show Pandoc commands in command line
                   output_file = file_name, # define directory where to store output file
                   clean = TRUE, # intermediate files are deleted
-                  output_dir = "G:/Risiko/Anwendungen/Report_Systemindikatoren/report/Archiv",
-                  encoding = "UTF-8") # select corect encoding
+                  output_dir = "../Output",
+                  encoding = "UTF-8") # select correct encoding
 
 ################################################################################
